02fb269de5cb3d0036a469d388668f77ccbce876
RomaLikhachev <likhachevroman1996@gmail.com>
2024-02-14 20:46:40 +0400

    LEETCODE-1: Two Sum

---
+++ LeetCode1/Program.cs
@@ -0,0 +1,35 @@
+﻿// https://leetcode.com/problems/two-sum/
+
+using System.Collections;
+
+int[] result = new Solution().TwoSum([1, 1, 7, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 11);
+Array.ForEach(result, Console.WriteLine);
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var result = new int[2];
+        var verifiedNumDict = new Dictionary<int, int>(nums.Length);
+
+        for (var index = 0; index < nums.Length; index++)
+        {
+            var value = nums[index];
+
+            var remain = target - value;
+
+            if (verifiedNumDict.ContainsKey(remain))
+            {
+                result[0] = index;
+                result[1] = verifiedNumDict[remain];
+                break;
+            }
+            else
+            {
+                verifiedNumDict.TryAdd(value, index);
+            }
+        }
+
+        return result;
+    }
+}
